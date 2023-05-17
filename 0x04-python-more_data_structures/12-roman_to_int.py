66debb129b3eb28c6c36ce29a4c2f0621c72e241
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-17 16:26:55 -0700

    12. Roman to Integer 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     if not isinstance(roman_string, str) or roman_string is None:
-        return None
+        return 0
     for index in range(0, len(roman_string)):
         if index < len(roman_string) - 1:
             letter1 = roman_string[index]
