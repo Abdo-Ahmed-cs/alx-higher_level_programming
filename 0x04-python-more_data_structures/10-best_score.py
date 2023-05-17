70544d4911870219ea89ad40196d06e3bdaae256
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-17 14:50:47 -0700

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        best_score_v = a_dictionary[next(iter(a_dictionary))]
+        for k, v in a_dictionary.items():
+            if best_score_v < v:
+                best_score_v = v
+        keys = [key for key, value in a_dictionary.items()
+                if value == best_score_v]
+        return keys[0]
