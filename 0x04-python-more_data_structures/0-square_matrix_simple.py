9d3a79cbc2d0c74f4d4acaf3b01b880e8a320c6c
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-17 11:55:04 -0700

    fixing bycodestyle error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda vertex: list(map(lambda number: number ** 2, vertex)), matrix))
+    new_matrix = list(
+        map(lambda vertex: list(
+            map(lambda number: number ** 2, vertex)), matrix))
     return new_matrix
