16f999b16de804ee8e90955c83fdb2f5bc0f7795
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-30 17:22:47 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defining a square class based on last task"""
+
+
+class Square:
+    """starting class Square"""
+
+    def __init__(self, size):
+        """initializing function
+
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
