9e55e1aa70be4459bc650221e9756f5112d4e102
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-06-19 06:35:25 -0700

    8. Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
+    else:
+        return res
