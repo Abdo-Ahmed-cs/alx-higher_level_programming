576c73beb41f2c86dc8b59d72be8bdd3b57a7675
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-06-19 05:23:49 -0700

    0. Safe list printing 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(1, x + 1):
+    for i in range(x + 1):
         try:
-            print(my_list[i - 1], end="")
+            print(my_list[i], end="")
         except IndexError:
-            i = i - 1
             break
     print("")
     return i
